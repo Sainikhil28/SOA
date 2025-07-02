db83363a5921c41f93d6e7f96a545d75e6111d9e
shanthosh-babu-R <shanthoshbabu.r@gmail.com>
2025-07-02 10:18:21 +0530

    Update AdminDashboard.js

--- hospital-insurance/src/components/AdminDashboard.js
+++ hospital-insurance/src/components/AdminDashboard.js
@@ -36,7 +36,7 @@ const AdminDashboard = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
 
-        <Link className="navbar-brand" to="/admin">AdminDashboard</Link>
+        <Link className="navbar-brand" to="/admin">Admin Dashboard</Link>
 
 
         <div className="collapse navbar-collapse">
